510d92ad9cf9bb8c77b0b8b56958ce7e184216e5
henrypaul2001 <henryp1.hp@googlemail.com>
2024-11-10 14:50:16 +0000

    Implemented surface area heuristic in bvh building

--- GLTrace/BVH.h
+++ GLTrace/BVH.h
@@ -107,12 +107,15 @@ private:
 			node.aabbMin = glm::min(node.aabbMin, Q);
 			node.aabbMin = glm::min(node.aabbMin, QU);
 			node.aabbMin = glm::min(node.aabbMin, QV);
-			node.aabbMin = glm::min(node.aabbMin, QUV);
 
 			node.aabbMax = glm::max(node.aabbMax, Q);
 			node.aabbMax = glm::max(node.aabbMax, QU);
 			node.aabbMax = glm::max(node.aabbMax, QV);
-			node.aabbMax = glm::max(node.aabbMax, QUV);
+
+			if (leafQuad.triangle_disk_id != 1u) { // if not a triangle
+				node.aabbMin = glm::min(node.aabbMin, QUV);
+				node.aabbMax = glm::max(node.aabbMax, QUV);
+			}
 		}
 		for (unsigned int first = node.firstSpherePrimitive, i = 0; i < node.spherePrimitiveCount; i++) {
 			const unsigned int sphereID = sphereIDs[first + i];
@@ -126,6 +129,164 @@ private:
 		}
 	}
 
+	float EvaluateSAH(const BVHNode& node, const int axis, const float pos, const std::vector<Quad>& quads, const std::vector<Sphere>& spheres) {
+		glm::vec3 leftAABBMin = glm::vec3(1e30f), leftAABBMax = glm::vec3(-1e30f);
+		glm::vec3 rightAABBMin = glm::vec3(1e30f), rightAABBMax = glm::vec3(-1e30f);
+		int leftCount = 0, rightCount = 0;
+		for (unsigned int i = 0; i < node.quadPrimitiveCount; i++) {
+			const Quad& quad = quads[quadIDs[node.firstQuadPrimitive + i]];
+			const glm::vec3& Q = quad.GetQ(), U = quad.GetU(), V = quad.GetV();
+			const glm::vec3 QU = Q + U;
+			const glm::vec3 QV = Q + V;
+			const glm::vec3 QUV = Q + U + V;
+
+			if (quad.GetCentre()[axis] < pos) {
+				leftCount++;
+
+				leftAABBMin = glm::min(leftAABBMin, Q);
+				leftAABBMin = glm::min(leftAABBMin, QU);
+				leftAABBMin = glm::min(leftAABBMin, QV);
+
+				leftAABBMax = glm::max(leftAABBMax, Q);
+				leftAABBMax = glm::max(leftAABBMax, QU);
+				leftAABBMax = glm::max(leftAABBMax, QV);
+
+				if (quad.triangle_disk_id != 1u) { // if not a triangle
+					leftAABBMin = glm::min(leftAABBMin, QUV);
+					leftAABBMax = glm::max(leftAABBMax, QUV);
+				}
+			}
+			else {
+				rightCount++;
+
+				rightAABBMin = glm::min(rightAABBMin, Q);
+				rightAABBMin = glm::min(rightAABBMin, QU);
+				rightAABBMin = glm::min(rightAABBMin, QV);
+
+				rightAABBMax = glm::max(rightAABBMax, Q);
+				rightAABBMax = glm::max(rightAABBMax, QU);
+				rightAABBMax = glm::max(rightAABBMax, QV);
+
+				if (quad.triangle_disk_id != 1u) { // if not a triangle
+					rightAABBMin = glm::min(rightAABBMin, QUV);
+					rightAABBMax = glm::max(rightAABBMax, QUV);
+				}
+			}
+		}
+		for (unsigned int i = 0; i < node.spherePrimitiveCount; i++) {
+			const Sphere& sphere = spheres[sphereIDs[node.firstSpherePrimitive + i]];
+			const float sphereRadius = sphere.Radius;
+			const glm::vec3 sphereMin = glm::vec3(sphere.Center) - glm::vec3(sphereRadius);
+			const glm::vec3 sphereMax = glm::vec3(sphere.Center) + glm::vec3(sphereRadius);
+
+			if (sphere.Center[axis] < pos) {
+				leftCount++;
+
+				leftAABBMin = glm::min(leftAABBMin, sphereMin);
+				leftAABBMax = glm::max(leftAABBMax, sphereMax);
+			}
+			else {
+				rightCount++;
+
+				rightAABBMin = glm::min(rightAABBMin, sphereMin);
+				rightAABBMax = glm::max(rightAABBMax, sphereMax);
+			}
+		}
+		const glm::vec3 leftExtent = leftAABBMax - leftAABBMin;
+		float leftBoxArea = leftExtent.x * leftExtent.y + leftExtent.y * leftExtent.z + leftExtent.z * leftExtent.x;
+		const glm::vec3 rightExtent = rightAABBMax - rightAABBMin;
+		float rightBoxArea = rightExtent.x * rightExtent.y + rightExtent.y * rightExtent.z + rightExtent.z * rightExtent.x;
+		float cost = leftCount * leftBoxArea + rightCount * rightBoxArea;
+		return cost > 0 ? cost : 1e30f;
+	}
+
+	void Subdivide(const unsigned int nodeID, const std::vector<Quad>& quads, const std::vector<Sphere>& spheres) {
+		BVHNode& node = tree[nodeID];
+
+		// Determine split axis using SAH
+		int bestAxis = -1;
+		float bestPos = 0.0f, bestCost = 1e30f;
+		for (int axis = 0; axis < 3; axis++) {
+			for (unsigned int i = 0; i < node.quadPrimitiveCount; i++) {
+				const Quad& quad = quads[quadIDs[node.firstQuadPrimitive + i]];
+				float candidatePos = quad.GetCentre()[axis];
+				float cost = EvaluateSAH(node, axis, candidatePos, quads, spheres);
+				if (cost < bestCost) {
+					bestPos = candidatePos;
+					bestAxis = axis;
+					bestCost = cost;
+				}
+			}
+			for (unsigned int i = 0; i < node.spherePrimitiveCount; i++) {
+				const Sphere& sphere = spheres[sphereIDs[node.firstSpherePrimitive + i]];
+				float candidatePos = sphere.Center[axis];
+				float cost = EvaluateSAH(node, axis, candidatePos, quads, spheres);
+				if (cost < bestCost) {
+					bestPos = candidatePos;
+					bestAxis = axis;
+					bestCost = cost;
+				}
+			}
+		}
+		int axis = bestAxis;
+		float splitPos = bestPos;
+
+		// Get parent area
+		const glm::vec3 e = node.aabbMax - node.aabbMin;
+		float parentArea = e.x * e.y + e.y * e.z + e.z * e.x;
+		float parentCost = (node.quadPrimitiveCount + node.spherePrimitiveCount) * parentArea;
+
+		if (bestCost >= parentCost) { return; } // Further splits will be detrimental. Return
+
+		// Split node quads
+		int quadI = node.firstQuadPrimitive;
+		int quadJ = quadI + node.quadPrimitiveCount - 1;
+		while (quadI <= quadJ) {
+			if (quads[quadIDs[quadI]].GetCentre()[bestAxis] < splitPos) { quadI++; }
+			else { std::swap(quadIDs[quadI], quadIDs[quadJ--]); }
+		}
+		// Split node spheres
+		int sphereI = node.firstSpherePrimitive;
+		int sphereJ = sphereI + node.spherePrimitiveCount - 1;
+		while (sphereI <= sphereJ) {
+			if (spheres[sphereIDs[sphereI]].Center[bestAxis] < splitPos) { sphereI++; }
+			else { std::swap(sphereIDs[sphereI], sphereIDs[sphereJ--]); }
+		}
+
+		int leftQuadCount = quadI - node.firstQuadPrimitive;
+		int leftSphereCount = sphereI - node.firstSpherePrimitive;
+
+		// Create child nodes
+		int leftChildID = ++nodesUsed;
+		int rightChildID = ++nodesUsed;
+		node.leftChild = leftChildID;
+
+		tree[leftChildID].firstQuadPrimitive = node.firstQuadPrimitive;
+		tree[leftChildID].quadPrimitiveCount = leftQuadCount;
+		tree[leftChildID].firstSpherePrimitive = node.firstSpherePrimitive;
+		tree[leftChildID].spherePrimitiveCount = leftSphereCount;
+		tree[leftChildID].padding1 = 0;
+		tree[leftChildID].padding2 = 0;
+		tree[leftChildID].padding3 = 0;
+		tree[rightChildID].firstQuadPrimitive = quadI;
+		tree[rightChildID].quadPrimitiveCount = node.quadPrimitiveCount - leftQuadCount;
+		tree[rightChildID].firstSpherePrimitive = sphereI;
+		tree[rightChildID].spherePrimitiveCount = node.spherePrimitiveCount - leftSphereCount;
+		tree[rightChildID].padding1 = 0;
+		tree[rightChildID].padding2 = 0;
+		tree[rightChildID].padding3 = 0;
+		node.quadPrimitiveCount = 0;
+		node.spherePrimitiveCount = 0;
+
+		UpdateNodeBounds(leftChildID, quads, spheres);
+		UpdateNodeBounds(rightChildID, quads, spheres);
+
+		// Recursive split
+		Subdivide(leftChildID, quads, spheres);
+		Subdivide(rightChildID, quads, spheres);
+	}
+
+	/*
 	void Subdivide(const unsigned int nodeID, const std::vector<Quad>& quads, const std::vector<Sphere>& spheres) {
 		BVHNode& node = tree[nodeID];
 		if (node.quadPrimitiveCount + node.spherePrimitiveCount <= 2) { return; }
@@ -220,6 +381,7 @@ private:
 		Subdivide(leftChildID, quads, spheres);
 		Subdivide(rightChildID, quads, spheres);
 	}
+	*/
 
 	unsigned int rootNodeID, nodesUsed, totalElements;
 	std::vector<BVHNode> tree;
