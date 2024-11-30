ad83afefb0446a4d1d40612df15e6ff2c198d82b
henrypaul2001 <henryp1.hp@googlemail.com>
2024-11-30 13:23:05 +0000

    Added some methods to GLTMath for composing and decomposing transformation matrices

--- GLTrace/GLTMath.h
+++ GLTrace/GLTMath.h
@@ -24,3 +24,29 @@ inline const glm::mat4 create_transform(const glm::vec3& translation, const glm:
 	transform = glm::translate(transform, translation);
 	return transform;
 }
+inline void decompose_transform(const glm::mat4& transform, glm::vec3& translation, glm::vec3& rotation, glm::vec3& scale) {
+	// Extract translation
+	translation = glm::vec3(transform[3]);
+
+	// Extract scale
+	scale = glm::vec3(
+		glm::length(glm::vec3(transform[0])),
+		glm::length(glm::vec3(transform[1])),
+		glm::length(glm::vec3(transform[2]))
+	);
+
+	// Remove scale from the matrix to isolate rotation
+	glm::mat4 rotationMatrix = transform;
+	rotationMatrix[0] /= scale.x;
+	rotationMatrix[1] /= scale.y;
+	rotationMatrix[2] /= scale.z;
+
+	// Extract Euler angles (rotation) from the rotation matrix
+	rotation = glm::eulerAngles(glm::quat_cast(rotationMatrix));
+}
+inline glm::mat4 compose_transform(const glm::vec3& translation, const glm::vec3& euler_rotation, const glm::vec3& scale) {
+	glm::mat4 transform = glm::translate(glm::mat4(1.0f), translation);
+	transform *= glm::mat4_cast(glm::quat(euler_rotation));
+	transform = glm::scale(transform, scale);
+	return transform;
+}
